0eb6d39a5791dd622f48be6984f8130193e506ca
gerganaDoicheva <mojeli_1@abv.bg>
2016-02-24 11:04:23 +0200

    Modul 02,Modul 01-dobavqne na komentari

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // definirane na promenlivi
+
+
+
+            // subirane na 4isla
+
+
+            
+            // pisane v konzolata
+
+
+
+
+        }
+    }
+}
